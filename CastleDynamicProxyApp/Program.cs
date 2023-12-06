498f943a50d85413450207b9ca1010ead3f6bcb7
muhammet demiralay <pepmuhammet.42@gmail.com>
2023-12-06 23:27:56 +0300

    Aspect oriented programing, manuel aspect created.

---
+++ CastleDynamicProxyApp/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Castle.DynamicProxy;
+
+Console.WriteLine("Hello, World!");
+
+
+var proxy = new ProxyGenerator();
+var aspect = proxy.CreateClassProxy<MyClass>(new MyInterceptorAspect());
+aspect.MyMethod();
+Console.ReadLine();
+
+
+public class MyInterceptorAspect : IInterceptor
+{
+    public void Intercept(IInvocation invocation)
+    {
+        Console.WriteLine("Before {0}", invocation.Method);
+        invocation.Proceed();
+        Console.WriteLine("After {0}", invocation.Method);
+    }
+}
+
+public class MyClass
+{
+    public virtual void MyMethod()
+    {
+        Console.WriteLine("MyMethod body");
+    }
+}
---
+++ ProxyApp/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+public interface IBusinessModule
+{
+    void Method();
+}
+
+public class BusinessModule : IBusinessModule
+{
+    public void Method()
+    {
+        Console.WriteLine("Method...");
+    }
+}
+
+public class BusinessModuleProxy : IBusinessModule
+{
+    private BusinessModule _realSubject;
+
+    public BusinessModuleProxy(BusinessModule realSubject)
+    {
+        _realSubject = realSubject;
+    }
+
+    public void Method()
+    {
+        Console.WriteLine("Before method");
+        _realSubject.Method();
+        Console.WriteLine("After method");
+    }
+}
